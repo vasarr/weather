449a287e8bad124c19538128140fb5a5f4549203
mango <panwenmingchn@hotmail.com>
2019-01-28 08:48:08 +0000

    Apply fixes from StyleCI

--- src/Weather.php
+++ src/Weather.php
@@ -3,11 +3,10 @@
  * Created by PhpStorm.
  * User: vasar
  * Date: 2019-01-22
- * Time: 00:07
+ * Time: 00:07.
  */
 
 namespace Vasar\Weather;
-
 
 use GuzzleHttp\Client;
 use Vasar\Weather\Exceptions\HttpException;
@@ -18,7 +17,6 @@ class Weather
     protected $key;
 
     protected $guzzleOptions = [];
-
 
     public function __construct($key)
     {
@@ -45,16 +43,16 @@ class Weather
         return $this->getWeather($city, 'all', $format);
     }
 
-    public function getWeather($city, $type='base', $format = 'json')
+    public function getWeather($city, $type = 'base', $format = 'json')
     {
         $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
 
         if (!in_array(strtolower($type), ['base', 'all'])) {
-            throw new InvalidAgrumentException("Invalid type value(base/all): " . $type);
+            throw new InvalidAgrumentException('Invalid type value(base/all): '.$type);
         }
 
         if (!in_array(strtolower($format), ['json', 'xml'])) {
-            throw new InvalidAgrumentException("Invalid response format: " . $format);
+            throw new InvalidAgrumentException('Invalid response format: '.$format);
         }
 
         $query = array_filter([
@@ -73,6 +71,5 @@ class Weather
         } catch (\Exception $e) {
             throw new HttpException($e->getMessage(), $e->getCode(), $e);
         }
-
     }
 }
